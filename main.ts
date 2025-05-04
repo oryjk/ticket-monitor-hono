4954a9acc69ef3e571c2433fedb1715c2fa1ef64
WangRui <wangrui@betalpha.com>
2025-05-04 11:10:55 +0800

    添加发送邮件功能

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
-import {Hono} from "hono";
-import {logger} from "hono/logger";
-import {cors} from "hono/cors";
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 import match from "./match/match_handler.ts";
 import order_handler from "./order/order_handler.ts";
 import user_handler from "./user/user_handler.ts";
 
 const app = new Hono();
+const root = new Hono();
+root.route("/match", match);
+root.route("/order", order_handler);
+root.route("/member", user_handler);
 
 app.use(logger());
 app.use("*", cors());
-app.route("/match", match);
-app.route("/order", order_handler);
-app.route("/member", user_handler);
+app.route("/ticket-member", root);
 
 Deno.serve(app.fetch);
