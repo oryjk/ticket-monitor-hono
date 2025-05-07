97e64e752d757e56833277ffc3a3ee0be3544747
WangRui <wangrui@betalpha.com>
2025-05-08 01:18:36 +0800

    添加 licence

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-import { Hono } from "hono";
-import { logger } from "hono/logger";
-import { cors } from "hono/cors";
+import {Hono} from "hono";
+import {logger} from "hono/logger";
+import {cors} from "hono/cors";
 import match from "./match/match_handler.ts";
 import order_handler from "./order/order_handler.ts";
 import user_handler from "./user/user_handler.ts";
@@ -13,6 +13,19 @@ root.route("/member", user_handler);
 
 app.use(logger());
 app.use("*", cors());
+app.use(async (c, next) => {
+  console.log("middleware 3 start");
+  console.log("Received URL:", c.req.url)
+  console.log("All query parameters detected by Hono:", c.req.query());
+  const license_key = c.req.query("licence_key");
+  if (license_key) {
+    await next();
+    console.log("middleware 3 end");
+    return;
+  }
+  return c.text("licence_key miss", 403);
+});
+
 app.route("/ticket-member", root);
 
 Deno.serve(app.fetch);
